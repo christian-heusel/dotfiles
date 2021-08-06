866884a606857d181a7916ac8c5b36d4d759a769
Christian Heusel <christian@heusel.eu>
2021-08-06 20:36:46 +0200

    fix the power scripts

--- roles/custom-scripts/files/my_scripts/reboot.sh
+++ roles/custom-scripts/files/my_scripts/reboot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 zenity --question --width="370" --height=20 --title "System Reboot" --timeout=5 --text "You pressed the system reboot shortcut. \nDo you want to proceed?" \
-&& sudo reboot
+    && reboot
 
