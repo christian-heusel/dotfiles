c1952a19523ab716cdb7d4bb4d7a554517a91401
Christian Heusel <christian@heusel.eu>
2021-08-12 11:12:59 +0200

    fix path to the rofi conf in gopass_selector.sh

--- roles/custom-scripts/files/my_scripts/gopass_selector.sh
+++ roles/custom-scripts/files/my_scripts/gopass_selector.sh
@@ -1,6 +1,8 @@
 #!/bin/env bash
 set -o nounset                              # Treat unset variables as an error
 
-gopass list --flat | rofi -dmenu -i -l 5 -p "gopass" -config "/home/chris/.config/rofi/rofi.conf" | \
-    tee >(xargs gopass show -c) >(awk '{b=$1" ins clipboard kopiert!"; print b}' | xargs -I "{}" notify-send "gopass" "{}")
+gopass list --flat | \
+  rofi -dmenu -i -l 5 -p "gopass" -config "/home/chris/.config/i3/rofi.conf" | \
+  tee >(xargs gopass show -c) \
+      >(awk '{b=$1" ins clipboard kopiert!"; print b}' | xargs -I "{}" notify-send "gopass" "{}")
 
