a18959e675c7e75fb75531a3cb95d0e402f72a13
Christian Heusel <christian@heusel.eu>
2021-08-31 16:24:35 +0200

    adapt to the changes that came with rofi 1.7.0

--- roles/custom-scripts/files/my_scripts/gopass_selector.sh
+++ roles/custom-scripts/files/my_scripts/gopass_selector.sh
@@ -2,7 +2,7 @@
 set -o nounset                              # Treat unset variables as an error
 
 gopass list --flat | \
-  rofi -dmenu -i -l 5 -p "gopass" -config "/home/chris/.config/i3/rofi.conf" | \
+  rofi -dmenu -i -l 5 -p "gopass" | \
   tee >(xargs gopass show -c) \
       >(awk '{b=$1" ins clipboard kopiert!"; print b}' | xargs -I "{}" notify-send "gopass" "{}")
 
