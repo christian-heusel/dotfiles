2b266aad4d69a9ca686427f874bfa226918bc30f
Christian Heusel <christian@heusel.eu>
2021-11-29 00:12:59 +0100

    disable the theme for `gopass_qr.sh`

--- roles/custom-scripts/files/my_scripts/gopass_qr.sh
+++ roles/custom-scripts/files/my_scripts/gopass_qr.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 set -o nounset                              # Treat unset variables as an error
 
-password=$(gopass list --flat | rofi -dmenu -i -l 5 -p "gopass->qr" -config "/home/chris/.config/rofi/rofi.conf" | xargs gopass show --password)
+password=$(gopass list --flat | rofi -dmenu -i -l 5 -p "gopass->qr" | xargs gopass show --password)
 tmpfile=$(mktemp /tmp/gopass-qr.XXXXXX)
 qrencode $password -o $tmpfile && sxiv -bpq -z 130 $tmpfile
 
