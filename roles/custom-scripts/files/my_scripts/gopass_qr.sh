f8ca9adac1d8c45e13ee519dae9b8714ad14f1f6
Christian Heusel <christian@heusel.eu>
2021-12-07 13:57:49 +0100

    quote the input data to qrencode

--- roles/custom-scripts/files/my_scripts/gopass_qr.sh
+++ roles/custom-scripts/files/my_scripts/gopass_qr.sh
@@ -3,6 +3,6 @@ set -o nounset                              # Treat unset variables as an error
 
 password=$(gopass list --flat | rofi -dmenu -i -l 5 -p "gopass->qr" | xargs gopass show --password)
 tmpfile=$(mktemp /tmp/gopass-qr.XXXXXX)
-qrencode $password --dpi=500 -s 20 -m 1 -o $tmpfile && sxiv -bpq -Z $tmpfile
+qrencode "$password" --dpi=500 -s 20 -m 1 -o $tmpfile && sxiv -bpq -Z $tmpfile
 
 rm -v $tmpfile
