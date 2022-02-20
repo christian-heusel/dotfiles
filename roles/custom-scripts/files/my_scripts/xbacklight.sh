9995186dc79486814be56e285d482c640ec0b082
Christian Heusel <christian@heusel.eu>
2022-02-21 00:23:17 +0100

    fix the brightness setting

--- roles/custom-scripts/files/my_scripts/xbacklight.sh
+++ roles/custom-scripts/files/my_scripts/xbacklight.sh
@@ -6,13 +6,13 @@ bl_level=$default
 
 if [ $1 == 1 ]
 then
-	xbacklight -inc $bl_level_step
+    brightnessctl set +$bl_level_step%
 elif [ $1 == 2 ]
 then
-	xbacklight -dec $bl_level_step
+    brightnessctl set $bl_level_step%-
 elif [ $1 == 3 ]
 then
-	xbacklight -set $bl_level_default
+    xbacklight set $bl_level_default%
 else
-	sleep 1
+    sleep 1
 fi
