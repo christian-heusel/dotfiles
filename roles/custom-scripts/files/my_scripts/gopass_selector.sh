65a65f7d400544729d10304693f8252224c25e55
Christian Heusel <christian@heusel.eu>
2023-07-31 00:26:09 +0200

    use the --nosync option for gopass in both scripts
    related to https://github.com/gopasspw/gopass/pull/2626

--- roles/custom-scripts/files/my_scripts/gopass_selector.sh
+++ roles/custom-scripts/files/my_scripts/gopass_selector.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 set -o nounset                              # Treat unset variables as an error
 
-gopass list --flat | \
+gopass --nosync list --flat | \
   rofi -dmenu -i -l 5 -p "gopass" | \
   tee >(xargs gopass show -c) \
       >(awk '{b=$1" ins clipboard kopiert!"; print b}' | xargs -I "{}" notify-send "gopass" "{}")
