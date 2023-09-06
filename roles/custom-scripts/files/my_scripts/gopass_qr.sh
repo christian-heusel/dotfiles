8311d234421eccc59bb1646e135ede7adddcd1f5
Christian Heusel <christian@heusel.eu>
2023-09-06 20:11:24 +0200

    moar --nosync for gopass in scripts

--- roles/custom-scripts/files/my_scripts/gopass_qr.sh
+++ roles/custom-scripts/files/my_scripts/gopass_qr.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 set -o nounset                              # Treat unset variables as an error
 
-password=$(gopass --nosync list --flat | rofi -dmenu -i -l 5 -p "gopass->qr" | xargs gopass show --password)
+password=$(gopass --nosync list --flat | rofi -dmenu -i -l 5 -p "gopass->qr" | xargs gopass --nosync show --password)
 tmpfile=$(mktemp /tmp/gopass-qr.XXXXXX)
 qrencode "$password" --dpi=500 -s 20 -m 1 -o $tmpfile && sxiv -bpq -Z $tmpfile
 
--- roles/custom-scripts/files/my_scripts/gopass_selector.sh
+++ roles/custom-scripts/files/my_scripts/gopass_selector.sh
@@ -3,6 +3,6 @@ set -o nounset                              # Treat unset variables as an error
 
 gopass --nosync list --flat | \
   rofi -dmenu -i -l 5 -p "gopass" | \
-  tee >(xargs gopass show -c) \
+  tee >(xargs gopass --nosync show -c) \
       >(awk '{b=$1" ins clipboard kopiert!"; print b}' | xargs -I "{}" notify-send "gopass" "{}")
 
